6932e53d4a64f96d212ff14060bb6f1f5614788e
Mark Abernathy <mbabernathy@gmail.com>
2019-04-27 18:29:50 -0400

    Fully implement initial spell functions

--- src/combat.js
+++ src/combat.js
@@ -31,6 +31,49 @@ function resolveMelee(attacker, defender) {
 }
 
 function resolveSpell(attacker, defender, spell) {
-    return {attacker: attacker, defender: defender, logMessage: ''};
+    // Deal with cost first
+    if (spell.cost.str) {
+        attacker.stats.str = attacker.stats.str - spell.cost.str;
+    }
+    if (spell.cost.def) {
+        attacker.stats.def = attacker.stats.def - spell.cost.def;
+    }
+    if (spell.cost.acc) {
+        attacker.stats.acc = attacker.stats.acc - spell.cost.acc;
+    }
+    if (spell.cost.eva) {
+        attacker.stats.eva = attacker.stats.eva - spell.cost.eva;
+    }
+    if (spell.cost.con) {
+        attacker.stats.con = attacker.stats.con - spell.cost.con;
+    }
+
+    // resolve spell effect & flavor text
+    let log;
+    if (spell.effect.damage) {
+        defender.hp = defender.hp - spell.effect.damage;
+        log = `${defender.name} is seared for ${spell.effect.damage} magical damage!`;
+    }
+    if (spell.effect.debuffStr) {
+        defender.stats.str = Math.max(defender.stats.str - spell.effect.debuffStr, 0);
+        log = `${defender.name} appears to struggle lifting their weapon`;
+    }
+    if (spell.effect.debuffDef) {
+        defender.stats.def = Math.max(defender.stats.def - spell.effect.debuffDef, 0);
+        log = `${defender.name} looks confused and is no longer bracing themselves against an attack`;
+    }
+    if (spell.effect.debuffAcc) {
+        defender.stats.acc = Math.max(defender.stats.acc - spell.effect.debuffAcc, 0);
+        log = `${defender.name} squints their eyes as they look off to your left`;
+    }
+    if (spell.effect.debuffEva) {
+        defender.stats.eva = Math.max(defender.stats.eva - spell.effect.debuffEva, 0);
+        log = `${defender.name} stumbles around as they loose their footing`;
+    }
+    if (spell.effect.heal) {
+        attacker.hp = attacker.stats.con;
+        log = "As your wounds close, you feel much better.  Kind of...";
+    }
+    return {attacker: attacker, defender: defender, logMessage: log};
 }
 export default {resolveMelee, resolveSpell};
